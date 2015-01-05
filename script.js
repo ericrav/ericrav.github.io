59e7e1b09147339ab88014cf0a6f298ef8de98db
ericrav <ericrabinowitz@me.com>
2015-01-05 10:46:59 -0500

    place holder homepage

--- script.js
+++ script.js
@@ -1,64 +1,88 @@
-$(document).keypress(function(e){
-	if (e.which == 103) {
-		$(".grid-overlay").toggle();
-		e.preventDefault();
-	}
-});
-var hideAbout = function() {
-	$(".avatar").stop().css("visibility", "");
-	$(".about-me").animate({"min-width":0}, {
-		step: function(now,fx){
-			$(this).css("-webkit-transform","rotateY("+(270+parseInt(now))+"deg)");
-			$(this).css("transform","rotateY("+(270+parseInt(now))+"deg)");
-		},
-		duration: 400,
-		complete: function(){
-			$(".avatar").animate({"min-width":0}, {
-				step: function(now,fx){
-					$(this).css("-webkit-transform","rotateY("+now+"deg)");
-					$(this).css("transform","rotateY("+now+"deg)");
-				},
-				duration: 400,
-				complete: function(){
-					$(".about-me").hide();
-					$(".about-me").removeClass("shown");
-				}
-			});
-		}
-	});
-}
-var showAbout = function() {
-	$(".avatar").stop();
-	$(".about-me").stop().show();
-	$(".avatar").animate({"min-width":90}, {
-		step: function(now,fx){
-			$(this).css("-webkit-transform","rotateY("+now+"deg)");
-			$(this).css("transform","rotateY("+now+"deg)");
-		},
-		duration: 400,
-		complete: function(){
-			$(".about-me").animate({"min-width":90}, {
-				step: function(now,fx){
-					$(this).css("-webkit-transform","rotateY("+(270+parseInt(now))+"deg)");
-					$(this).css("transform","rotateY("+(270+parseInt(now))+"deg)");
-				},
-				duration: 400,
-				complete: function(){
-					$(".avatar").css("visibility", "hidden");
-					$(".about-me").addClass("shown");
-					$(document).one("click", hideAbout);
-				}
-			});
-		}
-	});
-}
+var ready = true;
+
 $(document).ready(function(){
-	$("#about").click(function(e){
-		if ($(".about-me").hasClass("shown")) hideAbout();
-		else showAbout();
-		e.preventDefault();
+
+	$(".links a").click(function(e) {
+		e.stopPropagation();
 	});
-	$(".avatar .overlay").click(function(e){
-		if (!$(".about-me").hasClass("shown")) showAbout();
+	$("body").click(function(e){
+
+		if (ready) {
+			ready = false;
+			var x = e.pageX;
+			var y = e.pageY;
+			var windowW = $(window).width();
+			var windowH = $(window).height();
+			var wx = Math.min(x, windowW - x)
+			var wy = Math.min(y, windowH - y)
+			var limit = Math.sqrt(Math.pow(windowW - wx, 2) + Math.pow(windowH - wy, 2)) * 2;
+			if ($("#c1").is(":visible")) {
+				$("#c2").show();
+				$("#c1").css("z-index", 1);
+				$("#c2").css({
+					"z-index": 100,
+					top: y + "px",
+					left: x + "px",
+					width: 0,
+					height: 0
+				});
+				$("#c2 .name").css("opacity", 0);
+				$("#c2 .name").animate({
+					opacity: 1
+				});
+				var width = 0, height = 0;
+				var timer = setInterval(function() {
+					if (width > limit) {
+						clearInterval(timer);
+						ready = true;
+						$("body").css("background", "#171d25");
+						$("#c1").hide();
+					}
+					$("#c2").css({
+						width: width + "px",
+						height: height + "px",
+						top: y + "px",
+						left: x + "px"
+					});
+					width += 80;
+					height += 80;
+					x -= 40;
+					y -= 40;
+				}, 20)
+			} else {
+				$("#c1").show();
+				$("#c2").css("z-index", 1);
+				$("#c1").css({
+					"z-index": 100,
+					top: y + "px",
+					left: x + "px",
+					width: 0,
+					height: 0
+				});
+				$("#c1 .name").css("opacity", 0);
+				$("#c1 .name").animate({
+					opacity: 1
+				});
+				var width = 0, height = 0;
+				var timer = setInterval(function() {
+					if (width > limit) {
+						clearInterval(timer);
+						ready = true;
+						$("body").css("background", "#E8E2DA");
+						$("#c2").hide();
+					}
+					$("#c1").css({
+						width: width + "px",
+						height: height + "px",
+						top: y + "px",
+						left: x + "px"
+					});
+					width += 80;
+					height += 80;
+					x -= 40;
+					y -= 40;
+				}, 20)
+			}
+		}
 	});
 });
